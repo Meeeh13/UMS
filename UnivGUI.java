b9fc271c38e5ab12a1c9ab9979812a40345254d6
FlightfulBirb <hsorendain@gmail.com>
2025-11-26 18:41:53 +0800

    Add files via upload

---
+++ UnivGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.univgui;
+
+/**
+ *
+ * @author Hannah
+ */
+public class UnivGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
